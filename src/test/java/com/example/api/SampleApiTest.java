a9004e9ddbe4d5af0538769827d8285ae02e0caf
Ke1logG <2251656765@qq.com>
2026-08-06 21:35:28 +0800

    引入RestAssured和JUnit5对应的依赖及简单测试

--- src/test/java/com/example/api/SampleApiTest.java
+++ src/test/java/com/example/api/SampleApiTest.java
@@ -1,29 +1,60 @@
 package com.example.api;
 
-import io.restassured.RestAssured;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.path.json.JsonPath.from;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * 示例接口测试类
  */
 public class SampleApiTest {
 
-    @BeforeClass
-    public void setup() {
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
-    }
+   /* @BeforeAll
+    static void setup() {
+        io.restassured.RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }*/
 
     @Test
-    public void shouldGetPostSuccessfully() {
+    void testGet() {
+        String str = given()
+                //.header("Content-Type", "application/json")
+                //.param("phone",123455)
+                .header("Hello","kl")
+
+        .when()
+                .get("https://httpbin.ceshiren.com/get")
+        .then()
+                .statusCode(200)
+                //.body("origin",equalTo("14.127.39.122, 182.92.156.22"))
+                .log().all()
+                .extract().jsonPath().getString("headers.Hello");
+
+        System.out.println(str);
+
+        assertEquals("kl",str);
+
+    }
+    /*@Test
+    void testPost() {
+        String str = "{\"hellow\":\"123\"}";
         given()
+                //.header("Content-Type", "application/json")
+                .contentType("application/json")
+                .body(str)
+                .log().headers()
+                .log().body()
+
                 .when()
-                .get("/posts/1")
+                .get("https://httpbin.ceshiren.com/get")
                 .then()
                 .statusCode(200)
-                .body("id", equalTo(1));
-    }
+                .body("origin",equalTo("14.127.39.122, 182.92.156.22"));
+                //.log().all();
+
+    }*/
 }
